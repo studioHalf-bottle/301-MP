c8c4148edaed7e7a7be75baa04b491bd3012daec
studioHalf-bottle <68529942+studioHalf-bottle@users.noreply.github.com>
2020-07-24 00:50:46 +0900

    Add files via upload

--- @js/updateNumber.js
+++ @js/updateNumber.js
@@ -23,65 +23,65 @@ var countUpJS_options_hasDone = { startVal: 0, duration: 1.75, suffix: '명' },
 
 function redraw_graph() {
 	
-	var timeJsonURL = "http://3.34.208.197/crawling";
-//	$.getJSON( timeJsonURL, function(data) {
-//		
-//		number_hasDone = data.count_hasDone;
-//		number_left = number_toGo - number_hasDone;
-//		
-//		currentTime = new Date(data.time);
-//		days_left = days_left_constant - currentTime.getDate() - 31*( currentTime.getMonth()+1-7 );
-//		
-//		
-//		$("object#d-day-integer").attr('data', `@asset/image/number-${days_left.toString()}.svg`);
-//		$("span#currentTime").html( `2020-0${currentTime.getMonth()+1}-${addZero(currentTime.getDate())} ${addZero(currentTime.getHours())}:${addZero(currentTime.getMinutes())}:${addZero(currentTime.getSeconds())}` );
-//
-//
-//
-//		var percent_hasDone = number_hasDone / number_toGo * 100;
-//		var percent_left = number_left / number_toGo * 100;
-//
-//		setTimeout( function() {
-//			$("object#d-day-integer").attr({ width: "", height: ""});
-//
-//			$("div#leftToGo-section").css("height", percent_left+"%"); 
-//			$("div#hasDone-section").css("height", percent_hasDone+"%");
-//			$("span.number-hasDone").each( function(i, ele) {
-//				new CountUp(ele, number_hasDone, countUpJS_options_hasDone).start();
-//			});
-//			$("span.number-left").each( function(i, ele) {
-//				new CountUp(ele, number_left, countUpJS_options_left).start();
-//			});
-//		}, 200 );
-//	});
+	var timeJsonURL = "https://301-mp-json.s3.ap-northeast-2.amazonaws.com/result.json";
+	$.getJSON( timeJsonURL, function(data) {
+		
+		number_hasDone = data.count_hasDone;
+		number_left = number_toGo - number_hasDone;
+		
+		currentTime = new Date(data.time);
+		days_left = days_left_constant - currentTime.getDate() - 31*( currentTime.getMonth()+1-7 );
+		
+		
+		$("object#d-day-integer").attr('data', `@asset/image/number-${days_left.toString()}.svg`);
+		$("span#currentTime").html( `2020-0${currentTime.getMonth()+1}-${addZero(currentTime.getDate())} ${addZero(currentTime.getHours())}:${addZero(currentTime.getMinutes())}:${addZero(currentTime.getSeconds())}` );
+
+
+
+		var percent_hasDone = number_hasDone / number_toGo * 100;
+		var percent_left = number_left / number_toGo * 100;
+
+		setTimeout( function() {
+			$("object#d-day-integer").attr({ width: "", height: ""});
+
+			$("div#leftToGo-section").css("height", percent_left+"%"); 
+			$("div#hasDone-section").css("height", percent_hasDone+"%");
+			$("span.number-hasDone").each( function(i, ele) {
+				new CountUp(ele, number_hasDone, countUpJS_options_hasDone).start();
+			});
+			$("span.number-left").each( function(i, ele) {
+				new CountUp(ele, number_left, countUpJS_options_left).start();
+			});
+		}, 200 );
+	});
 	
-	number_hasDone = number_hasDone_nonAutomatic;
-	number_left = number_toGo - number_hasDone;
-
-	currentTime = currentTime_nonAutomatic;
-	days_left = days_left_constant - currentTime.getDate() - 31*( currentTime.getMonth()+1-7 );
-
-
-	$("object#d-day-integer").attr('data', `@asset/image/number-${days_left.toString()}.svg`);
-	$("span#currentTime").html( `2020-0${currentTime.getMonth()+1}-${addZero(currentTime.getDate())} ${addZero(currentTime.getHours())}:${addZero(currentTime.getMinutes())}:${addZero(currentTime.getSeconds())}` );
-
-
-
-	var percent_hasDone = number_hasDone / number_toGo * 100;
-	var percent_left = number_left / number_toGo * 100;
-
-	setTimeout( function() {
-		$("object#d-day-integer").attr({ width: "", height: ""});
-
-		$("div#leftToGo-section").css("height", percent_left+"%"); 
-		$("div#hasDone-section").css("height", percent_hasDone+"%");
-		$("span.number-hasDone").each( function(i, ele) {
-			new CountUp(ele, number_hasDone, countUpJS_options_hasDone).start();
-		});
-		$("span.number-left").each( function(i, ele) {
-			new CountUp(ele, number_left, countUpJS_options_left).start();
-		});
-	}, 200 );
+//	number_hasDone = number_hasDone_nonAutomatic;
+//	number_left = number_toGo - number_hasDone;
+//
+//	currentTime = currentTime_nonAutomatic;
+//	days_left = days_left_constant - currentTime.getDate() - 31*( currentTime.getMonth()+1-7 );
+//
+//
+//	$("object#d-day-integer").attr('data', `@asset/image/number-${days_left.toString()}.svg`);
+//	$("span#currentTime").html( `2020-0${currentTime.getMonth()+1}-${addZero(currentTime.getDate())} ${addZero(currentTime.getHours())}:${addZero(currentTime.getMinutes())}:${addZero(currentTime.getSeconds())}` );
+//
+//
+//
+//	var percent_hasDone = number_hasDone / number_toGo * 100;
+//	var percent_left = number_left / number_toGo * 100;
+//
+//	setTimeout( function() {
+//		$("object#d-day-integer").attr({ width: "", height: ""});
+//
+//		$("div#leftToGo-section").css("height", percent_left+"%"); 
+//		$("div#hasDone-section").css("height", percent_hasDone+"%");
+//		$("span.number-hasDone").each( function(i, ele) {
+//			new CountUp(ele, number_hasDone, countUpJS_options_hasDone).start();
+//		});
+//		$("span.number-left").each( function(i, ele) {
+//			new CountUp(ele, number_left, countUpJS_options_left).start();
+//		});
+//	}, 200 );
 }
 	
 
